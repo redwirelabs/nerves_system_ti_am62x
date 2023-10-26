59b9e068ebbcbd29669e92040c335001b1627e6f
abelino <abelino.romo@gmail.com>
2023-10-26 14:03:59 -0700

    Use upstream buildroot ti-k3-image-gen

--- package/ti-k3-image-gen/ti-k3-image-gen.mk
+++ package/ti-k3-image-gen/ti-k3-image-gen.mk
@@ -11,39 +11,39 @@ TI_K3_IMAGE_GEN_LICENSE = BSD-3-Clause
 TI_K3_IMAGE_GEN_LICENSE_FILES = LICENSE
 TI_K3_IMAGE_GEN_INSTALL_IMAGES = YES
 
+# - ti-k3-image-gen is used to build tiboot3.bin, using the
+#   r5-u-boot-spl.bin file from the ti-k3-r5-loader package. Hence the
+#   dependency on ti-k3-r5-loader.
+# - the ti-k3-image-gen makefiles seem to need some feature from Make
+#   v4.0, similar to u-boot.
 TI_K3_IMAGE_GEN_DEPENDENCIES = \
 	host-arm-gnu-toolchain \
-	ti-linux-firmware \
+	host-python3 \
+	host-openssl \
+	host-uboot-tools \
 	ti-k3-r5-loader-next \
-	host-ti-k3-secdev
+	ti-k3-boot-firmware \
+	$(BR2_MAKE_HOST_DEPENDENCY)
 
-ifeq ($(BR2_PACKAGE_TI_K3_IMAGE_GEN_FW_TYPE_TIFS),y)
-TI_K3_IMAGE_GEN_FW_TYPE = $(call qstrip,"ti-fs")
-else ifeq ($(BR2_PACKAGE_TI_K3_IMAGE_GEN_FW_TYPE_TISCI),y)
-TI_K3_IMAGE_GEN_FW_TYPE = $(call qstrip,"ti-sci")
-else
-$(error No TI K3 Image Gen firmware type set)
-endif
-
+TI_K3_IMAGE_GEN_FW_TYPE = $(call qstrip,$(BR2_PACKAGE_TI_K3_IMAGE_GEN_FW_TYPE))
 TI_K3_IMAGE_GEN_SOC = $(call qstrip,$(BR2_PACKAGE_TI_K3_IMAGE_GEN_SOC))
-TI_K3_IMAGE_GEN_SOC_TYPE = $(call qstrip,$(BR2_PACKAGE_TI_K3_IMAGE_GEN_SOC_TYPE))
-TI_K3_IMAGE_GEN_CONFIG = $(call qstrip,$(BR2_PACKAGE_TI_K3_IMAGE_GEN_CONFIG))
+TI_K3_IMAGE_GEN_SECTYPE = $(call qstrip,$(BR2_PACKAGE_TI_K3_IMAGE_GEN_SECTYPE))
 
-TI_K3_IMAGE_GEN_SYSFW = $(TI_K3_IMAGE_GEN_FW_TYPE)-firmware-$(TI_K3_IMAGE_GEN_SOC)-$(TI_K3_IMAGE_GEN_SOC_TYPE).bin
+TI_K3_IMAGE_GEN_SYSFW = $(TI_K3_IMAGE_GEN_FW_TYPE)-firmware-$(TI_K3_IMAGE_GEN_SOC)-$(TI_K3_IMAGE_GEN_SECTYPE).bin
 define TI_K3_IMAGE_GEN_CONFIGURE_CMDS
 	cp $(BINARIES_DIR)/ti-sysfw/$(TI_K3_IMAGE_GEN_SYSFW) $(@D)
 endef
 
 define TI_K3_IMAGE_GEN_BUILD_CMDS
+	$(TARGET_MAKE_ENV) \
 	$(BR2_MAKE) -C $(@D) \
 		SOC=$(TI_K3_IMAGE_GEN_SOC) \
-		SOC_TYPE=$(TI_K3_IMAGE_GEN_SOC_TYPE) \
-		CONFIG=$(TI_K3_IMAGE_GEN_CONFIG) \
+		SOC_TYPE=$(TI_K3_IMAGE_GEN_SECTYPE) \
+		CONFIG=evm \
 		CROSS_COMPILE=$(HOST_DIR)/bin/arm-none-eabi- \
 		SBL=$(BINARIES_DIR)/r5-u-boot-spl.bin \
 		O=$(@D)/tmp \
-		BIN_DIR=$(@D) \
-		TI_SECURE_DEV_PKG=$(HOST_TI_K3_SECDEV_DIR)
+		BIN_DIR=$(@D)
 endef
 
 define TI_K3_IMAGE_GEN_INSTALL_IMAGES_CMDS
