9b53fd61b7534f427363e957287eaf06fd44d879
abelino <abelino.romo@gmail.com>
2023-10-26 14:00:42 -0700

    Use upstream buildroot ti-k3-boot-firmware instead

--- package/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
+++ package/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
@@ -1,20 +1,20 @@
 ################################################################################
 #
-# ti-linux-firmware
+# ti-k3-boot-firmware
 #
 ################################################################################
 
-TI_LINUX_FIRMWARE_VERSION = 08.06.00.007
-TI_LINUX_FIRMWARE_SITE = https://git.ti.com/cgit/processor-firmware/ti-linux-firmware/snapshot
-TI_LINUX_FIRMWARE_SOURCE = ti-linux-firmware-$(TI_LINUX_FIRMWARE_VERSION).tar.gz
-TI_LINUX_FIRMWARE_INSTALL_IMAGES = YES
-TI_LINUX_FIRMWARE_LICENSE = TI Proprietary
-TI_LINUX_FIRMWARE_LICENSE_FILES = LICENCE.ti
+TI_K3_BOOT_FIRMWARE_VERSION = 08.06.00.007
+TI_K3_BOOT_FIRMWARE_SITE = https://git.ti.com/cgit/processor-firmware/ti-linux-firmware/snapshot
+TI_K3_BOOT_FIRMWARE_SOURCE = ti-linux-firmware-$(TI_K3_BOOT_FIRMWARE_VERSION).tar.gz
+TI_K3_BOOT_FIRMWARE_INSTALL_IMAGES = YES
+TI_K3_BOOT_FIRMWARE_LICENSE = TI Proprietary
+TI_K3_BOOT_FIRMWARE_LICENSE_FILES = LICENSE.ti
 
-UBOOT_PRE_BUILD_HOOKS += TI_LINUX_FIRMWARE_RDEPEND_CMD
+UBOOT_PRE_BUILD_HOOKS += TI_K3_BOOT_FIRMWARE_RDEPEND_CMD
 
-define TI_LINUX_FIRMWARE_RDEPEND_CMD
-	$(MAKE) ti-linux-firmware -C $(BASE_DIR)
+define TI_K3_BOOT_FIRMWARE_RDEPEND_CMD
+	$(MAKE) ti-k3-boot-firmware -C $(BASE_DIR)
 endef
 
 TI_DM_FW_DIR = ti-dm/am62xx
@@ -29,7 +29,7 @@ UBOOT_MAKE_OPTS += DM=$(BINARIES_DIR)/$(TI_DM_FW_DIR)/$(TI_DM_FW_FILE)
 
 # Alternative name? am62-main-r5f0_0-fw
 
-define TI_LINUX_FIRMWARE_INSTALL_IMAGES_CMDS
+define TI_K3_BOOT_FIRMWARE_INSTALL_IMAGES_CMDS
 	cp -dpfr $(@D)/ti-sysfw $(BINARIES_DIR)/
 	cp -dpfr $(@D)/ti-dm $(BINARIES_DIR)/
 endef
